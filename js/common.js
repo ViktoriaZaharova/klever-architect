b51325aa111e6e68d3696b98403d55c40b214cc4
Viktoria Zaharova <zaharova181114@gmail.com>
2024-10-03 06:06:26 +0300

    pc version

--- js/common.js
+++ js/common.js
@@ -1,2 +1,15 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+$('.projects-slider').slick({
+  slidesToShow: 1,
+  fade: true,
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
+$('.team-slider').slick({
+  slidesToShow: 3,
+  appendArrows: '.team-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev slick-arrow-dark"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next slick-arrow-dark"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
